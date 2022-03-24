2109900abff5b07fc0a78d2e351d7db6ff19298b
Minsung Kim <minsunki@c1r4s1.42seoul.kr>
2022-03-24 14:32:48 +0900

    format updated

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 11:44:58 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/14 12:34:13 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/03/24 14:31:37 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ Zombie::Zombie(std::string name): _name(name) {}
 Zombie::~Zombie(void)
 {
 	std::cout
-	<< "<" << _name << "> "
-	<< "died. (deleted)" << std::endl;
+	<< _name
+	<< " died. (deleted)" << std::endl;
 }
 
 void	Zombie::announce(void)
 {
 	std::cout
-	<< "<" << _name << "> "
+	<< _name << ": "
 	<< "BraiiiiiiinnnzzzZ..." << std::endl;
 }
